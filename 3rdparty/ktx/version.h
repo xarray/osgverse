eb44cae08bea9c244882fe3f00eb5bad63e34d81
xarray <wangray84@foxmail.com>
2023-05-14 16:50:37 +0800

    Add ktx as dependency

---
+++ 3rdparty/ktx/version.h
@@ -0,0 +1,11 @@
+/*
+// [API version]
+@par API Version
+v4.0
+// [API version]
+// [Code version]
+v4.0
+// [Code version]
+*/
+#define LIBKTX_VERSION v4.0
+#define LIBKTX_DEFAULT_VERSION v4.0.__default__
