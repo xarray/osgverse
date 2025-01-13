a00acf853e59aef0307b83370d243c84c6a3eb5a
Wang Rui <wangray84@foxmail.com>
2025-01-13 18:12:47 +0800

    Add test code in particle_u3d_test

--- tests/particle_u3d_test.cpp
+++ tests/particle_u3d_test.cpp
@@ -37,28 +37,89 @@ int main(int argc, char** argv)
 
     osg::ref_ptr<osg::Geode> particleNode = new osg::Geode;
     {
-        osg::ref_ptr<osgVerse::ParticleSystemU3D> ps = new osgVerse::ParticleSystemU3D;
-        ps->setTexture(osgVerse::createTexture2D(
-            osgDB::readImageFile(BASE_DIR + "/textures/foam_texture_sheet.png")));
-        ps->setParticleType(osgVerse::ParticleSystemU3D::PARTICLE_Billboard);
-        ps->setGravityScale(0.0); ps->setMaxParticles(1200);
-        ps->setDuration(3.0); ps->setAspectRatio(16.0 / 9.0);
-        ps->setStartLifeRange(osg::Vec2(1.0f, 2.0f));
-        ps->setStartSizeRange(osg::Vec2(2.0f, 5.0f));
-        ps->setStartSpeedRange(osg::Vec2(0.0f, 0.0f));
-        ps->setEmissionCount(osg::Vec2(100.0f, 0.0f));
-        ps->setEmissionShape(osgVerse::ParticleSystemU3D::EMIT_Box);
-        ps->setEmissionShapeCenter(osg::Vec3(0.0f, 6.0f, -2.5f));
-        ps->setEmissionShapeValues(osg::Vec4(12.0f, 6.0f, 6.0f, 0.0f));
-        ps->getColorPerTime()[0.0f] = osg::Vec4(0.4, 0.4, 0.4f, 0.0f);
-        ps->getColorPerTime()[0.5f] = osg::Vec4(0.4, 0.4, 0.4f, 0.1f);
-        ps->getColorPerTime()[1.0f] = osg::Vec4(0.4, 0.4, 0.4f, 0.0f);
-        ps->setTextureSheetTiles(osg::Vec2(8.0f, 8.0f));
-        ps->setTextureSheetValues(osg::Vec4(32.0f, 0.0f, 0.0f, 0.0f));
-        ps->setBlendingType(osgVerse::ParticleSystemU3D::BLEND_Additive);
-        ps->linkTo(particleNode.get(), true,
-                   osgDB::readShaderFile(osg::Shader::VERTEX, SHADER_DIR + "particles.vert.glsl"),
-                   osgDB::readShaderFile(osg::Shader::FRAGMENT, SHADER_DIR + "particles.frag.glsl"));
+        osg::ref_ptr<osg::Image> image = osgDB::readImageFile(BASE_DIR + "/textures/foam_texture_sheet.dds");
+        osg::ref_ptr<osg::Shader> vs = osgDB::readShaderFile(osg::Shader::VERTEX, SHADER_DIR + "particles.vert.glsl");
+        osg::ref_ptr<osg::Shader> fs = osgDB::readShaderFile(osg::Shader::FRAGMENT, SHADER_DIR + "particles.frag.glsl");
+
+        osg::ref_ptr<osgVerse::ParticleSystemU3D> ps0 = new osgVerse::ParticleSystemU3D;
+        ps0->setTexture(osgVerse::createTexture2D(image.get()));
+        ps0->setParticleType(osgVerse::ParticleSystemU3D::PARTICLE_Billboard);
+        ps0->setGravityScale(0.0); ps0->setMaxParticles(1200);
+        ps0->setDuration(3.0); ps0->setAspectRatio(16.0 / 9.0);
+        ps0->setStartLifeRange(osg::Vec2(1.0f, 2.0f));
+        ps0->setStartSizeRange(osg::Vec2(2.0f, 5.0f));
+        ps0->setStartSpeedRange(osg::Vec2(0.0f, 0.0f));
+        ps0->setEmissionCount(osg::Vec2(100.0f, 0.0f));
+        ps0->setEmissionShape(osgVerse::ParticleSystemU3D::EMIT_Box);
+        ps0->setEmissionShapeCenter(osg::Vec3(0.0f, 6.0f, -2.5f));
+        ps0->setEmissionShapeValues(osg::Vec4(12.0f, 6.0f, 6.0f, 0.0f));
+        ps0->getColorPerTime()[0.0f] = osg::Vec4(1.0f, 0.4f, 0.4f, 0.0f);
+        ps0->getColorPerTime()[0.5f] = osg::Vec4(1.0f, 0.4f, 0.4f, 0.1f);
+        ps0->getColorPerTime()[1.0f] = osg::Vec4(1.0f, 0.4f, 0.4f, 0.0f);
+        ps0->setTextureSheetTiles(osg::Vec2(8.0f, 8.0f));
+        ps0->setTextureSheetValues(osg::Vec4(32.0f, 0.0f, 0.0f, 0.0f));
+        ps0->setBlendingType(osgVerse::ParticleSystemU3D::BLEND_Additive);
+        ps0->linkTo(particleNode.get(), false, vs.get(), fs.get());
+
+        osg::ref_ptr<osgVerse::ParticleSystemU3D> ps1 = new osgVerse::ParticleSystemU3D;
+        ps1->setTexture(osgVerse::createTexture2D(image.get()));
+        ps1->setParticleType(osgVerse::ParticleSystemU3D::PARTICLE_Billboard);
+        ps1->setGravityScale(0.0); ps1->setMaxParticles(1200);
+        ps1->setDuration(3.0); ps1->setAspectRatio(16.0 / 9.0);
+        ps1->setStartLifeRange(osg::Vec2(1.0f, 2.0f));
+        ps1->setStartSizeRange(osg::Vec2(2.0f, 5.0f));
+        ps1->setStartSpeedRange(osg::Vec2(0.0f, 0.0f));
+        ps1->setEmissionCount(osg::Vec2(100.0f, 0.0f));
+        ps1->setEmissionShape(osgVerse::ParticleSystemU3D::EMIT_Box);
+        ps1->setEmissionShapeCenter(osg::Vec3(0.0f, -6.0f, -2.5f));
+        ps1->setEmissionShapeValues(osg::Vec4(12.0f, 6.0f, 6.0f, 0.0f));
+        ps1->getColorPerTime()[0.0f] = osg::Vec4(0.4f, 1.0f, 0.4f, 0.0f);
+        ps1->getColorPerTime()[0.5f] = osg::Vec4(0.4f, 1.0f, 0.4f, 0.1f);
+        ps1->getColorPerTime()[1.0f] = osg::Vec4(0.4f, 1.0f, 0.4f, 0.0f);
+        ps1->setTextureSheetTiles(osg::Vec2(8.0f, 8.0f));
+        ps1->setTextureSheetValues(osg::Vec4(32.0f, 0.0f, 0.0f, 0.0f));
+        ps1->setBlendingType(osgVerse::ParticleSystemU3D::BLEND_Additive);
+        ps1->linkTo(particleNode.get(), false, vs.get(), fs.get());
+
+        osg::ref_ptr<osgVerse::ParticleSystemU3D> ps2 = new osgVerse::ParticleSystemU3D;
+        ps2->setTexture(osgVerse::createTexture2D(image.get()));
+        ps2->setParticleType(osgVerse::ParticleSystemU3D::PARTICLE_Billboard);
+        ps2->setGravityScale(0.0); ps2->setMaxParticles(1200);
+        ps2->setDuration(3.0); ps2->setAspectRatio(16.0 / 9.0);
+        ps2->setStartLifeRange(osg::Vec2(1.0f, 2.0f));
+        ps2->setStartSizeRange(osg::Vec2(2.0f, 5.0f));
+        ps2->setStartSpeedRange(osg::Vec2(0.0f, 0.0f));
+        ps2->setEmissionCount(osg::Vec2(100.0f, 0.0f));
+        ps2->setEmissionShape(osgVerse::ParticleSystemU3D::EMIT_Box);
+        ps2->setEmissionShapeCenter(osg::Vec3(6.0f, 0.0f, -2.5f));
+        ps2->setEmissionShapeValues(osg::Vec4(12.0f, 6.0f, 6.0f, 0.0f));
+        ps2->getColorPerTime()[0.0f] = osg::Vec4(0.4f, 0.4f, 1.0f, 0.0f);
+        ps2->getColorPerTime()[0.5f] = osg::Vec4(0.4f, 0.4f, 1.0f, 0.1f);
+        ps2->getColorPerTime()[1.0f] = osg::Vec4(0.4f, 0.4f, 1.0f, 0.0f);
+        ps2->setTextureSheetTiles(osg::Vec2(8.0f, 8.0f));
+        ps2->setTextureSheetValues(osg::Vec4(32.0f, 0.0f, 0.0f, 0.0f));
+        ps2->setBlendingType(osgVerse::ParticleSystemU3D::BLEND_Additive);
+        ps2->linkTo(particleNode.get(), false, vs.get(), fs.get());
+
+        osg::ref_ptr<osgVerse::ParticleSystemU3D> ps3 = new osgVerse::ParticleSystemU3D;
+        ps3->setTexture(osgVerse::createTexture2D(image.get()));
+        ps3->setParticleType(osgVerse::ParticleSystemU3D::PARTICLE_Billboard);
+        ps3->setGravityScale(0.0); ps3->setMaxParticles(1200);
+        ps3->setDuration(3.0); ps3->setAspectRatio(16.0 / 9.0);
+        ps3->setStartLifeRange(osg::Vec2(1.0f, 2.0f));
+        ps3->setStartSizeRange(osg::Vec2(2.0f, 5.0f));
+        ps3->setStartSpeedRange(osg::Vec2(0.0f, 0.0f));
+        ps3->setEmissionCount(osg::Vec2(100.0f, 0.0f));
+        ps3->setEmissionShape(osgVerse::ParticleSystemU3D::EMIT_Box);
+        ps3->setEmissionShapeCenter(osg::Vec3(-6.0f, 0.0f, -2.5f));
+        ps3->setEmissionShapeValues(osg::Vec4(12.0f, 6.0f, 6.0f, 0.0f));
+        ps3->getColorPerTime()[0.0f] = osg::Vec4(0.4f, 0.4f, 0.4f, 0.0f);
+        ps3->getColorPerTime()[0.5f] = osg::Vec4(0.4f, 0.4f, 0.4f, 0.1f);
+        ps3->getColorPerTime()[1.0f] = osg::Vec4(0.4f, 0.4f, 0.4f, 0.0f);
+        ps3->setTextureSheetTiles(osg::Vec2(8.0f, 8.0f));
+        ps3->setTextureSheetValues(osg::Vec4(32.0f, 0.0f, 0.0f, 0.0f));
+        ps3->setBlendingType(osgVerse::ParticleSystemU3D::BLEND_Additive);
+        ps3->linkTo(particleNode.get(), true, vs.get(), fs.get());
     }
 
     osg::ref_ptr<osg::MatrixTransform> particleMT = new osg::MatrixTransform;
