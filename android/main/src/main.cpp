8be8128d70d8385f1585211254fcc9fe4a819bf4
wangray84 <wangray84@foxmail.com>
2025-06-01 16:55:45 +0800

    Improve Android command-line compilation process and make test SDL application run!

--- android/main/src/main.cpp
+++ android/main/src/main.cpp
@@ -1,17 +1,45 @@
-#include "SDL.h"
+#include <osg/io_utils>
+#include <osg/ComputeBoundsVisitor>
+#include <osg/Texture2D>
+#include <osg/MatrixTransform>
+#include <osgDB/ReadFile>
+#include <osgDB/WriteFile>
+#include <osgGA/StateSetManipulator>
+#include <osgGA/TrackballManipulator>
+#include <osgUtil/CullVisitor>
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+#include <pipeline/SkyBox.h>
+#include <pipeline/Pipeline.h>
+#include <pipeline/LightModule.h>
+#include <pipeline/ShadowModule.h>
+#include <pipeline/Utilities.h>
+#include <readerwriter/Utilities.h>
+#include <iostream>
+#include <sstream>
+#include <android/log.h>
+#include <SDL.h>
+
+USE_OSG_PLUGINS()
+USE_VERSE_PLUGINS()
+USE_GRAPICSWINDOW_IMPLEMENTATION(SDL)
 
 int SDL_main(int argc, char* argv[])
 {
+    osgVerse::globalInitialize(argc, argv);
+    osg::setNotifyHandler(new osgVerse::ConsoleHandler);
+
     bool quit = false;
     SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+    __android_log_print(ANDROID_LOG_VERBOSE, "osgVerse", "SDL_main() started");
 
     // Create an application window with the following settings:
     SDL_Window* window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
+        "PBR Example",                     // window title
         SDL_WINDOWPOS_UNDEFINED,           // initial x position
         SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
+        480,                               // width, in pixels
+        800,                               // height, in pixels
         SDL_WINDOW_OPENGL                  // flags - see below
     );
 
@@ -35,6 +63,7 @@ int SDL_main(int argc, char* argv[])
         }
     }
 
+    __android_log_print(ANDROID_LOG_VERBOSE, "osgVerse", "SDL_main() finished");
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
