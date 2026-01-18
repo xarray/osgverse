f4962ea81b39c948be2f51ea4faf00f4c58ee3e4
Wang Rui <wangray84@foxmail.com>
2026-01-18 12:14:48 +0800

    Fix compilation errors on MooreThreads AIBook

--- wrappers/generic_osg/UpdateCallback.cpp
+++ wrappers/generic_osg/UpdateCallback.cpp
@@ -4,5 +4,4 @@ using namespace osgVerse;
 REGISTER_OBJECT_WRAPPER(UpdateCallback,
                         new osg::DrawableUpdateCallback,
                         osg::DrawableUpdateCallback,
-                        "osg::UpdateCallback",
                         "osg::Object osg::Callback osg::UpdateCallback") {}
