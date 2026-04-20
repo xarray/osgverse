50dd83d82eb5162673d5218b7db7bda4283897df
Wang Rui <wangray84@foxmail.com>
2026-04-20 13:18:01 +0800

    Remove gainput dependency because it is useless in current windowing framework Add KeyboardCacher for caching and reusing keyboard events

--- ui/Utilities.h
+++ ui/Utilities.h
@@ -4,10 +4,28 @@
 #include <osg/Transform>
 #include <osg/Geometry>
 #include <osg/Camera>
+#include <osgGA/EventQueue>
+#include <unordered_map>
 #include <string>
 
 namespace osgVerse
 {
+
+    /** Keyboard state buffering manager */
+    class KeyboardCacher : public osg::Referenced
+    {
+    public:
+        static KeyboardCacher* instance();
+        void advance(const osgGA::GUIEventAdapter& ea);
+
+        bool isKeyDown(int key) const;
+        bool anyKeyDown(std::initializer_list<int> keys) const;
+        bool allKeyDown(std::initializer_list<int> keys) const;
+
+    protected:
+        KeyboardCacher() {}
+        std::unordered_map<int, bool> _keyStates;
+    };
 
     /** File and message dialog object */
     class FileDialog : public osg::Referenced
